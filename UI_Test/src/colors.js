1952518b884dc4d9b4366f07afd61d0fe314ddd9
QuirkiDigital <46080052+QuirkiDigital@users.noreply.github.com>
2021-12-28 12:57:19 +0800

    Commit UI
    Updated UI
    File Structure
    added placeholder data and images

---
+++ UI_Test/src/colors.js
@@ -0,0 +1,11 @@
+const COLORS = {
+    white: '#fff',
+    dark: '#000',
+    red: '#F52A2A',
+    light: '#F1F1F1',
+    // green: '#00B761',
+    green: '#1778F2',
+    blue: '#1778F2',
+  };
+  
+  export default COLORS;
---
+++ UI_Test/src/plants.js
@@ -0,0 +1,60 @@
+const plants = [
+    {
+      id: 1,
+      name: 'Oneplus Nord',
+      price: '226.71',
+      like: true,
+    //   img: require('../assets/plant1.png'),
+      about:
+        'Oneplus Nord N100',
+    },
+  
+    {
+      id: 2,
+      name: 'OnePlus Nord 2',
+      price: '799.00',
+      like: false,
+    //   img: require('../assets/plant2.png'),
+      about:
+        'OnePlus Nord 2 5G',
+    },
+    {
+      id: 3,
+      name: 'OnePlus 8T',
+      price: '1,099.00',
+      like: false,
+    //   img: require('../assets/plant3.png'),
+      about:
+      'OnePlus 8T',
+    },
+  
+    {
+      id: 4,
+      name: 'OPPO Find X3 Pro',
+      price: '1,020.00',
+      like: true,
+    //   img: require('../assets/plant4.png'),
+      about:
+      'OPPO Find X3 Pro',
+    },
+    {
+      id: 5,
+      name: 'Mi 11 T Pro',
+      price: '768.00',
+      like: true,
+    //   img: require('../assets/plant5.png'),
+      about:
+      'Mi 11 T Pro',
+    },
+    {
+      id: 6,
+      name: 'I PHONE 13 MINI',
+      price: '1,099.00',
+      like: false,
+    //   img: require('../assets/plant6.png'),
+      about:
+      'I PHONE 13 MINI',
+    },
+  ];
+  
+  export default plants;
