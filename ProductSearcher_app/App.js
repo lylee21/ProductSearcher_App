f20669a64fc6a4af205ccd0a573bfe887c28e663
ImtheTed <31605638+teedy8023mars@users.noreply.github.com>
2021-12-29 23:07:56 +0800

    something

--- ProductSearcher_app/App.js
+++ ProductSearcher_app/App.js
@@ -1,24 +1,24 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import "react-native-gesture-handler";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 const Stack = createStackNavigator();
-import HomeScreen from './src/HomeScreen';
-import DetailsSceeen from './src/DetailsScreen';
-import {StatusBar} from 'react-native';
-import COLORS from './src/colors';
+import HomeScreen from "./src/HomeScreen";
+import DetailsSceeen from "./src/DetailsScreen";
+import { StatusBar } from "react-native";
+import COLORS from "./src/colors";
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <StatusBar barStyle="dark-content" backgroundColor={COLORS.white} />
-      <Stack.Navigator screenOptions={{header: () => null}}>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsSceeen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+			<StatusBar barStyle="dark-content" backgroundColor={COLORS.white} />
+			<Stack.Navigator screenOptions={{ header: () => null }}>
+				<Stack.Screen name="Home" component={HomeScreen} />
+				<Stack.Screen name="Details" component={DetailsSceeen} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 };
 
 export default App;
