efa6fb18b0f096299cf8128335129c6a1c31c4e0
QuirkiDigital <46080052+QuirkiDigital@users.noreply.github.com>
2021-12-30 13:28:35 +0800

    Code cleanup
    cleaned up redundant code

--- ProductSearcher_app/src/DetailsScreen.js
+++ ProductSearcher_app/src/DetailsScreen.js
@@ -6,6 +6,7 @@ import COLORS from "./colors";
 const DetailsScreen = ({ navigation, route }) => {
 	const plant = route.params;
 
+<<<<<<< Updated upstream
 	return (
 		<SafeAreaView
 			style={{
@@ -161,6 +162,111 @@ const style = StyleSheet.create({
 		borderTopLeftRadius: 25,
 		borderBottomLeftRadius: 25,
 	},
+=======
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: COLORS.white,
+      }}>
+      <View style={style.header}>
+        <Icon name="arrow-back" size={28} onPress={() => navigation.goBack()} />
+      </View>
+      <View style={style.imageContainer}>
+        <Image source={placeholder} style={{resizeMode: 'contain', flex: 1}} />
+      </View>
+      <View style={style.detailsContainer}>
+        <View
+          style={{
+            marginLeft: 20,
+            flexDirection: 'row',
+            alignItems: 'flex-end',
+          }}>
+        </View>
+        <View
+          style={{
+            marginLeft: 20,
+            marginTop: 20,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}>
+          <Text style={{fontSize: 30, fontWeight: 'bold'}}>{product.name}</Text>
+          <View style={style.priceTag}>
+            <Text
+              style={{
+                marginLeft: 15,
+                color: COLORS.white,
+                fontWeight: 'bold',
+                fontSize: 16,
+              }}>
+              ${product.price}
+            </Text>
+          </View>
+        </View>
+        <View style={{paddingHorizontal: 20, marginTop: 10}}>
+          <Text style={{fontSize: 20, fontWeight: 'bold'}}>Description</Text>
+          <Text
+            style={{
+              color: 'grey',
+              fontSize: 16,
+              lineHeight: 22,
+              marginTop: 10,
+            }}>
+            {product.about}
+          </Text>
+          <View
+            style={{
+              marginTop: 20,
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+            }}>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}>
+
+            </View>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const style = StyleSheet.create({
+  header: {
+    paddingHorizontal: 20,
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  imageContainer: {
+    flex: 0.45,
+    marginTop: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  detailsContainer: {
+    flex: 0.55,
+    backgroundColor: COLORS.light,
+    marginHorizontal: 7,
+    marginBottom: 7,
+    borderRadius: 20,
+    marginTop: 30,
+    paddingTop: 30,
+  },
+  priceTag: {
+    backgroundColor: COLORS.blue,
+    width: 100,
+    height: 40,
+    justifyContent: 'center',
+    paddingLeft:5,
+    borderTopLeftRadius: 25,
+    borderBottomLeftRadius: 25,
+  },
+>>>>>>> Stashed changes
 });
 
 export default DetailsScreen;
--- ProductSearcher_app/src/HomeScreen.js
+++ ProductSearcher_app/src/HomeScreen.js
@@ -7,6 +7,7 @@ import plants from "./plants";
 const width = Dimensions.get("window").width / 2 - 30;
 import placeholder from "../assets/default-product-image.png";
 
+<<<<<<< Updated upstream
 const HomeScreen = ({ navigation }) => {
 	// const [catergoryIndex, setCategoryIndex] = React.useState(0);
 
@@ -188,5 +189,131 @@ const style = StyleSheet.create({
 		justifyContent: "center",
 		alignItems: "center",
 	},
+=======
+const HomeScreen = ({navigation}) => {
+
+  const Card = ({product}) => {
+    return (
+      <TouchableOpacity
+        activeOpacity={0.8}
+        onPress={() => navigation.navigate('Details', product)}>
+        <View style={style.card}>
+          <View style={{alignItems: 'flex-end'}}>
+            <View
+              style={{
+                width: 30,
+                height: 30,
+                borderRadius: 20,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+            </View>
+          </View>
+
+          <View
+            style={{
+              height: 100,
+              alignItems: 'center',
+            }}>
+            <Image
+              source={placeholder}
+              style={{flex: 1, resizeMode: 'contain'}}
+            />
+          </View>
+
+          <Text style={{fontWeight: 'bold', fontSize: 17, marginTop: 10}}>
+            {product.name}
+          </Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              marginTop: 5,
+            }}>
+            <Text style={{fontSize: 19, fontWeight: 'bold'}}>
+              ${product.price}
+            </Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+  return (
+    <SafeAreaView
+      style={{flex: 1, paddingHorizontal: 20, backgroundColor: COLORS.white}}>
+      <View style={style.header}>
+        <View>
+          <Text style={{fontSize: 20, fontWeight: 'bold'}}>Find the best deal with</Text>
+          <Text style={{fontSize: 38, color: COLORS.blue, fontWeight: 'bold'}}>
+            Product Searcher
+          </Text>
+        </View>
+      </View>
+      <View style={{marginTop: 15, flexDirection: 'row'}}>
+        <View style={style.searchContainer}>
+          <TextInput placeholder="Search your product" style={style.input} />
+        </View>
+        <View style={style.searchBtn}>
+          <Icon name="search" size={30} color={COLORS.white} />
+        </View>
+      </View>
+      <FlatList
+        columnWrapperStyle={{justifyContent: 'space-between'}}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          marginTop: 10,
+          paddingBottom: 50,
+        }}
+        numColumns={2}
+        data={products}
+        renderItem={({item}) => {
+          return <Card product={item} />;
+        }}
+      />
+    </SafeAreaView>
+  );
+};
+
+const style = StyleSheet.create({
+  card: {
+    height: 225,
+    backgroundColor: COLORS.light,
+    width,
+    marginHorizontal: 2,
+    borderRadius: 10,
+    marginBottom: 20,
+    padding: 15,
+  },
+  header: {
+    marginTop: 30,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  searchContainer: {
+    height: 50,
+    backgroundColor: COLORS.light,
+    borderRadius: 10,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 15,
+  },
+  input: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    flex: 1,
+    color: COLORS.dark,
+    marginLeft: 20,
+  },
+  searchBtn: {
+    marginLeft: 10,
+    height: 50,
+    width: 50,
+    borderRadius: 10,
+    backgroundColor: COLORS.blue,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+>>>>>>> Stashed changes
 });
 export default HomeScreen;
